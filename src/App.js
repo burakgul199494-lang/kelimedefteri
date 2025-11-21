baa39d7590f34f9cefdb150d8e6e7a810f8cb1e8
burakgul199494-lang <burakgul199494@gmail.com>
2025-11-21 14:29:27 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -2908,7 +2908,7 @@ export default function App() {
             </div>
 
             <h1 className="text-3xl font-extrabold text-slate-800 tracking-tight">
-              Kelime Defteri 2
+              Kelime Defteri 3
             </h1>
             <p className="text-slate-500 mt-2 text-sm">
               Merhaba, <span className="font-medium text-indigo-600">{user.displayName || user.email}</span>
