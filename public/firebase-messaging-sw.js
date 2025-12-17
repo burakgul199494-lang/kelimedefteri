983fd0b2148d56620e7fc7fb154015edbb9c2c9e
burakgul199494-lang <burakgul199494@gmail.com>
2025-12-17 12:06:38 +0300

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+// public/firebase-messaging-sw.js
+
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpdcEZIaCzf4fvnrk9LD0D6WIuXWO30NA",
+  authDomain: "burak-a9c07.firebaseapp.com",
+  projectId: "burak-a9c07",
+  storageBucket: "burak-a9c07.firebasestorage.app",
+  messagingSenderId: "922162845642",
+  appId: "1:922162845642:web:75b579cbe5f46983996133",
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Arka plan mesajı:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/logo192.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
