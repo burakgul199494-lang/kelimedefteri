0aa0cbc14a3e39e5becf97ce5b76631791a34745
burakgul199494-lang <burakgul199494@gmail.com>
2025-12-25 08:43:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "cropperjs/dist/cropper.css";   // 🔥 EKLEDİĞİMİZ SATIR
+import "./styles.css"; // 🔥 EN ÖNEMLİ KISIM: Stilleri projeye dahil ediyoruz
+import "cropperjs/dist/cropper.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
